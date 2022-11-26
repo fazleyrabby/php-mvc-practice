2f8f183c11a672f0dc350cd741d7dc1c4784fca9
fazleyrabby <fazley111@gmail.com>
2022-11-26 13:39:14 +0600

    authentication,usermodel

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 use App\Controllers\AuthController;
 use App\Controllers\SiteController;
 use App\Core\App;
+use App\Models\User;
 use Dotenv\Dotenv;
 
 require_once __DIR__."/../vendor/autoload.php";
@@ -14,6 +15,7 @@ $dotenv->load();
 $root = dirname(__DIR__);
 
 $config=[
+    'userClass' => User::class,
     'db' => [
         'dsn' => $_ENV['DB_DSN'],
         'user' => $_ENV['DB_USER'],
@@ -37,6 +39,7 @@ $route->get('/login', [AuthController::class, 'login']);
 $route->post('/login', [AuthController::class, 'login']);
 $route->get('/register', [AuthController::class, 'register']);
 $route->post('/register', [AuthController::class, 'register']);
+$route->get('/logout', [AuthController::class, 'logout']);
 
 $app->run();
 
